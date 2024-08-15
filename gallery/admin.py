44e2c09f5f95c1e4eb1729e5f40140724db33e94
kukanji <111495470+kukanji@users.noreply.github.com>
2024-08-15 03:47:14 +0000

    homeモデルからinstagram, twitterの項目を削除

--- gallery/admin.py
+++ gallery/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Home
 
 class HomeAdmin(admin.ModelAdmin):
-    list_display = ('artist', 'description', 'instagram', 'twitter', 'created_at', 'updated_at')
+    list_display = ('artist', 'description', 'created_at', 'updated_at')
 
 admin.site.register(Home, HomeAdmin)
--- gallery/models.py
+++ gallery/models.py
@@ -5,10 +5,21 @@ class Home(models.Model):
     artist = models.ForeignKey(User, on_delete=models.CASCADE)
     art = models.ImageField(upload_to='home_art')
     description = models.TextField()
-    instagram = models.URLField()
-    twitter = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.artist.username
+    
+# class Gallery(models.Model):
+#     artist = models.ForeignKey(User, on_delete=models.CASCADE)
+#     title = models.CharField(max_length=255)
+#     description = models.TextField()
+#     instagram = models.URLField()
+#     twitter = models.URLField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return self.title
+    
