2991f3050a6b47e39813e604e75fcf7397a73396
kukanji <111495470+kukanji@users.noreply.github.com>
2024-08-15 04:23:25 +0000

    adminファイルにGalleyAdminを追加

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Home
+from .models import Home, Gallery
 
 class HomeAdmin(admin.ModelAdmin):
-    list_display = ('artist', 'description', 'created_at', 'updated_at')
+    list_display = ('artist', 'art', 'description', 'instagram', 'twitter', 'created_at', 'updated_at')
+
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ('artist', 'title', 'description', 'created_at', 'updated_at')
+
 
 admin.site.register(Home, HomeAdmin)
+admin.site.register(Gallery, GalleryAdmin)
