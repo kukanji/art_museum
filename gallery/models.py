f9735e6e39d03c2aeee26433688f557aae01e313
kukanji <111495470+kukanji@users.noreply.github.com>
2024-08-15 04:03:57 +0000

    Galleryモデルの追加。また、Homeモデルのinstagram, twitter項目の再追加と複数の項目にnull, blankの属性を適宜追加。

--- gallery/models.py
+++ gallery/models.py
@@ -3,23 +3,23 @@ from django.contrib.auth.models import User
 
 class Home(models.Model):
     artist = models.ForeignKey(User, on_delete=models.CASCADE)
-    art = models.ImageField(upload_to='home_art')
-    description = models.TextField()
+    art = models.ImageField(upload_to='home_art', null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    instagram = models.URLField(null=True, blank=True)
+    twitter = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.artist.username
     
-# class Gallery(models.Model):
-#     artist = models.ForeignKey(User, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=255)
-#     description = models.TextField()
-#     instagram = models.URLField()
-#     twitter = models.URLField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+class Gallery(models.Model):
+    artist = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
     
