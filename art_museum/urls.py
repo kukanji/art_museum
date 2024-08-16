9d02604f295b1ac734772f217065d1914ff98561
kukanji <111495470+kukanji@users.noreply.github.com>
2024-08-16 14:16:39 +0000

    media、staticフォルダを参照するためのstaticの追加

--- art_museum/urls.py
+++ art_museum/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gallery.urls')),
-]
+    *static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT),
+    *static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    # path()はプログラムファイル,static()はCSS等の静的ファイルを呼び出す
+    # staticはリスト型が返ってくるから"*"で展開する
+]# settings内のmedia_urlが合致した際にdocument_rootで定義した画像を呼び出す
