b4690f1e6b8fe9e39a5c5b4cbc6ffeed6ab35fa0
kukanji <111495470+kukanji@users.noreply.github.com>
2024-08-16 06:38:35 +0000

    多対多のArtモデル追加

--- gallery/models.py
+++ gallery/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Home(models.Model):
     artist = models.ForeignKey(User, on_delete=models.CASCADE)
-    art = models.ImageField(upload_to='home_art', null=True, blank=True)
+    art = models.ImageField(upload_to='art_home', null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     instagram = models.URLField(null=True, blank=True)
     twitter = models.URLField(null=True, blank=True)
@@ -17,6 +17,18 @@ class Gallery(models.Model):
     artist = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class Art(models.Model):
+    gallery = models.ManyToManyField(Gallery)
+    art = models.ImageField(upload_to='art_gallery')
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    like_sum = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
