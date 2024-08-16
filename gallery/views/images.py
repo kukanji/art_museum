ffe85e89bf53245011b2d3d6a201824c0d5530f0
kukanji <111495470+kukanji@users.noreply.github.com>
2024-08-16 14:28:23 +0000

    multipleの綴りを間違えていた

--- gallery/views/images.py
+++ gallery/views/images.py
@@ -22,11 +22,11 @@ def artist_home(request, username):
 def gallery(request, username, title):
     get_single_artist = User.objects.get(username=username)
     get_multiple_gallery = Gallery.objects.filter(artist = get_single_artist)
-    description = Gallery.objects.get(title = title).description
+    get_single_gallery = Gallery.objects.get(title = title)
     #get_single_gallery = Gallery.objects.filter(title = title).values('description')
-    get_mutiple_art = Art.objects.filter(title = title)
+    get_multiple_art = Art.objects.filter(gallery = get_single_gallery)
     return render(request, 'gallery.html', {'username': username,
                                             'title': title,
                                             'get_multiple_gallery': get_multiple_gallery,
-                                            'description': description,
-                                            'get_mutiple_art': get_mutiple_art})
+                                            'get_single_gallery': get_single_gallery,
+                                            'get_multiple_art': get_multiple_art})
