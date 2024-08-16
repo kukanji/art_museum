5c47bb9e6ade0fddd9218018efc3cbb7aebae25f
kukanji <111495470+kukanji@users.noreply.github.com>
2024-08-16 08:03:14 +0000

    artist_homeページの作成

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views.images import top_page, artist_home
+from .views.images import top_page, artist_home, gallery
 
 urlpatterns = [
     path('', top_page, name='top_page'),
     path('<str:username>/', artist_home, name='artist_home'),
+    path('gallery/<str:username>/', gallery, name='gallery'),
 ]
--- gallery/views/images.py
+++ gallery/views/images.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from gallery.models import Home, Gallery, Art
 import logging
 
 logger = logging.getLogger(__name__)
 
 def top_page(request):
-    artist_list = User.objects.all()
-    return render(request, 'top_page.html', {'artist_list': artist_list})
+    get_all_artist = User.objects.all()
+    return render(request, 'top_page.html', {'get_all_artist': get_all_artist})
 
 def artist_home(request, username):
-    artist = User.objects.get(username=username)
-    return render(request, 'artist_home.html', {'artist': artist})
+    print("username: ", username)
+    logger.info("username: %s", username)
+    get_single_artist = User.objects.get(username=username)
+    get_single_artist_home = Home.objects.get(artist = get_single_artist) # getの引数のartistはForeignKeyであり、idフィールドがnumberを期待しているため、usernameではなくget_single_artistを使う必要がある。
+    get_multiple_artist_gallery = Gallery.objects.filter(artist = get_single_artist)
+    return render(request, 'artist_home.html', {'username': username,
+                                                'get_single_artist_home': get_single_artist_home,
+                                                'get_multiple_artist_gallery': get_multiple_artist_gallery})
+
+def gallery(request, username):
+    get_single_artist = User.objects.get(username=username)
+    return render(request, 'gallery.html', {'username': username})
