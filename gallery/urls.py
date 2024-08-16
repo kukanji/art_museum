8248e8c44f66ae5442a29bedd5ad0054a7f616c4
kukanji <111495470+kukanji@users.noreply.github.com>
2024-08-16 13:33:42 +0000

    galleryページの作成

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,5 +4,5 @@ from .views.images import top_page, artist_home, gallery
 urlpatterns = [
     path('', top_page, name='top_page'),
     path('<str:username>/', artist_home, name='artist_home'),
-    path('gallery/<str:username>/', gallery, name='gallery'),
+    path('<str:username>/<str:title>/', gallery, name='gallery'),
 ]
--- gallery/views/images.py
+++ gallery/views/images.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from gallery.models import Home, Gallery, Art
-import logging
+#import logging
 
-logger = logging.getLogger(__name__)
+#logger = logging.getLogger(__name__)
 
 def top_page(request):
     get_all_artist = User.objects.all()
@@ -11,14 +11,22 @@ def top_page(request):
 
 def artist_home(request, username):
     print("username: ", username)
-    logger.info("username: %s", username)
+    #logger.info("username: %s", username)
     get_single_artist = User.objects.get(username=username)
     get_single_artist_home = Home.objects.get(artist = get_single_artist) # getの引数のartistはForeignKeyであり、idフィールドがnumberを期待しているため、usernameではなくget_single_artistを使う必要がある。
-    get_multiple_artist_gallery = Gallery.objects.filter(artist = get_single_artist)
+    get_multiple_gallery = Gallery.objects.filter(artist = get_single_artist)
     return render(request, 'artist_home.html', {'username': username,
                                                 'get_single_artist_home': get_single_artist_home,
-                                                'get_multiple_artist_gallery': get_multiple_artist_gallery})
+                                                'get_multiple_gallery': get_multiple_gallery})
 
-def gallery(request, username):
+def gallery(request, username, title):
     get_single_artist = User.objects.get(username=username)
-    return render(request, 'gallery.html', {'username': username})
+    get_multiple_gallery = Gallery.objects.filter(artist = get_single_artist)
+    description = Gallery.objects.get(title = title).description
+    #get_single_gallery = Gallery.objects.filter(title = title).values('description')
+    get_mutiple_art = Art.objects.filter(title = title)
+    return render(request, 'gallery.html', {'username': username,
+                                            'title': title,
+                                            'get_multiple_gallery': get_multiple_gallery,
+                                            'description': description,
+                                            'get_mutiple_art': get_mutiple_art})
