8b851504b09442c1bcab86ecf9454c937ce2b862
kukanji <111495470+kukanji@users.noreply.github.com>
2024-09-01 15:33:21 +0000

    axiosでAPIを取得した

--- artmuseum/settings.py
+++ artmuseum/settings.py
@@ -77,8 +77,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
+    'http://127.0.0.1:5173',
+    'http://localhost:5173',
 ]
 
 ROOT_URLCONF = 'artmuseum.urls'
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import axios from 'axios';
 import './App.css'
 
 function App() {
+
+  axios
+      .get(`http://127.0.0.1:8000/artgallery/home/`)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
 
   return (
     <>
