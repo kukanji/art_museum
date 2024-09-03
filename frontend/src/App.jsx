6d621181288ef4a7023485a6466953c9048326d9
kukanji <111495470+kukanji@users.noreply.github.com>
2024-09-03 06:45:40 +0000

    不要な部分の削除

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,19 @@ import { Top } from './pages/Top'
 import { Home } from './pages/Home'
 
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await axios.get('http://127.0.0.1:8000/artgallery/home/');
-        console.log(response.data);
-        setData(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getData();
-  }, []);
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     try {
+  //       const response = await axios.get('http://127.0.0.1:8000/artgallery/home/');
+  //       console.log(response.data);
+  //       setData(response.data);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+  //   getData();
+  // }, []);
 
   return (
     <>
@@ -31,11 +31,11 @@ function App() {
             <Route path="/home" element={<Home />} />
           </Routes>
       </BrowserRouter>
-      <ul>
+      {/* <ul>
         {data.map((item) => (
           <li key={item.id}>{item.artist}</li>
         ))}
-      </ul>
+      </ul> */}
     </>
   )
 }
