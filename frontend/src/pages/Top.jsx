fa24a7a37a0b852748e00771ded5585d91508c77
kukanji <111495470+kukanji@users.noreply.github.com>
2024-09-03 06:45:58 +0000

    コメントアウトを外すと今く動作しない

--- frontend/src/pages/Top.jsx
+++ frontend/src/pages/Top.jsx
@@ -1,7 +1,28 @@
 export const Top = () => {
+//     const [data, setData] = useState([]);
+//     useEffect(() => {
+//     const getData = async () => {
+//       try {
+//         const response = await axios.get('http://127.0.0.1:8000/artgallery/home/');
+//         console.log(response.data);
+//         setData(response.data);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+//     getData();
+//   }, []);
+  
     return (
-        <div>
-        <h1>TopPageを表示しています。</h1>
-        </div>
+        <>
+            <div>
+            <h1>TopPageを表示しています。</h1>
+            </div>
+            {/* <ul>
+            {data.map((item) => (
+            <li key={item.id}>{item.artist}</li>
+            ))}
+            </ul> */}
+        </> 
     );
 };
