ef87ad6e02bf8882b536d7598cd7634a91f35a3a
kukanji <111495470+kukanji@users.noreply.github.com>
2024-09-04 05:37:14 +0000

    クエリパラメータでフィルタリングしてデータを取得しようとしたがうまく行かなかった

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,34 +1,42 @@
 import { useParams } from "react-router-dom";
 import { useSearchParams } from "react-router-dom"; //書き換え
 import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 export const Gallery = () => {
     const params = useParams();
     const [searchParams] = useSearchParams(); //書き換え
     const galleryId = params.gallery_id;
-    const galleryTitle = searchParams.get("gallery_title");
-    console.log(galleryId);
-    console.log(galleryTitle);
+    const artistId = searchParams.get("artist_id"); //書き換え
+    console.log(`artistId:${artistId}`);
     const [galleryData, setGalleryData] = useState([]);
     const [artData, setArtData] = useState([]);
-    // useEffect(() => {
-    // const getData = async () => {
-    //   try {
-    //     const galleryResponse = await axios.get(`${import.meta.env.VITE_API_URL}/gallery/${params.artist_id}/`);
-    //     const artResponse = await axios.get(`${import.meta.env.VITE_API_URL}/art/`);
-    //     console.log(galleryResponse.data);
-    //     console.log(artResponse.data);
-    //     setGalleryData(galleryResponse.data);
-    //     setArtData(artResponse.data);
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // };
+
+    useEffect(() => {
+    const getData = async () => {
+        try {
+        const oneGalleryResponse = await axios.get(`${import.meta.env.VITE_API_URL}/gallery/${galleryId}/`);
+        // const allGalleryResponse = await axios.get(`${import.meta.env.VITE_API_URL}/gallery?artist_id=${artistId}/`);
+        const multipleGalleriesResponse = await axios.get(`${import.meta.env.VITE_API_URL}/gallery/`, {
+            params: {
+              artist_id : artistId
+            }
+          });
+        console.log(oneGalleryResponse.data);
+        console.log(multipleGalleriesResponse.data);
+        setGalleryData(oneGalleryResponse.data);
+        setArtData(multipleGalleriesResponse.data);
+        } catch (error) {
+        console.error(error);
+        }
+    };
+    getData();
+    }, []);
     return (
         <div>
         Gallery.jsxのページを表示しています。
         <p>galleryId:{galleryId}</p>
-        <p>galleryTitle:{galleryTitle}</p>
+        <p>artistId:{artistId}</p>
         </div>
     );
 };
