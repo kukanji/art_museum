9b73c6da604187be8fd233d3be7ecf658a59f3ee
kukanji <111495470+kukanji@users.noreply.github.com>
2024-09-16 12:59:22 +0000

    add nginx "80"port

--- artmuseum/settings.py
+++ artmuseum/settings.py
@@ -83,6 +83,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
     'http://localhost:5173',
+    'http://127.0.0.1:80',
+    'http://localhost:80',
     'http://artmuseum.y-kanji.com:5173',
     'http://ec2-34-224-66-81.compute-1.amazonaws.com:5173',
 ]
