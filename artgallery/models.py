dbe475ae4ac89a99253c4ed791e173983d76c84d
kanji <kanji8147@gmail.com>
2025-02-27 18:37:26 +0900

    サムネイル用のフィールドを追加して、画像の解像度を一律変換から個別変換に変更

--- artgallery/models.py
+++ artgallery/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from PIL import Image
 from io import BytesIO
 from django.core.files import File
+from django.core.files.base import ContentFile
 
 class Home(models.Model):
     artist = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -34,10 +35,12 @@ class Gallery(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+
 class Art(models.Model):
     gallery = models.ManyToManyField(Gallery)
-    art = models.ImageField(upload_to='art_gallery')
+    art = models.ImageField(upload_to='arts/')
+    thumbnail = models.ImageField(upload_to='arts/thumbnails/', blank=True)
     title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     like_sum = models.IntegerField(default=0)
@@ -45,16 +48,67 @@ class Art(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
-        img = Image.open(self.art)
-        img = img.convert('RGB')
-        output = BytesIO()
-        img.save(output, format='JPEG', quality=20)
-        output.seek(0)
-        self.art = File(output, self.art.name.split('.')[0] + '.jpg')
+        if self.art:
+            # オリジナル画像の処理
+            img = Image.open(self.art)
+            img = img.convert('RGB')
+            
+            # オリジナル画像のリサイズ処理
+            max_original_size = 2000
+            if img.width > max_original_size or img.height > max_original_size:
+                ratio = min(max_original_size/img.width, max_original_size/img.height)
+                new_size = (int(img.width * ratio), int(img.height * ratio))
+                img = img.resize(new_size, Image.Resampling.LANCZOS)
+            
+            # オリジナル画像の保存
+            output = BytesIO()
+            img.save(output, format='JPEG', quality=85)
+            output.seek(0)
+            self.art = File(output, self.art.name)
+
+            # サムネイルの作成
+            thumb_size = 400
+            thumb_img = Image.open(self.art)
+            # サムネイルのリサイズ処理
+            ratio = min(thumb_size/thumb_img.width, thumb_size/thumb_img.height)
+            new_size = (int(thumb_img.width * ratio), int(thumb_img.height * ratio))
+            thumb_img = thumb_img.resize(new_size, Image.Resampling.LANCZOS)
+            
+            # サムネイルの保存
+            thumb_output = BytesIO()
+            thumb_img.save(thumb_output, format='JPEG', quality=75)
+            thumb_output.seek(0)
+            
+            # サムネイルのファイル名を生成
+            thumb_filename = f'thumb_{self.art.name.split("/")[-1]}'
+            self.thumbnail.save(thumb_filename, ContentFile(thumb_output.getvalue()), save=False)
+
         super(Art, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
+    
+# class Art(models.Model):
+#     gallery = models.ManyToManyField(Gallery)
+#     art = models.ImageField(upload_to='arts/')
+#     thumbnail = models.ImageField(upload_to='arts/thumbnails/', blank=True)
+#     title = models.CharField(max_length=255)
+#     description = models.TextField(null=True, blank=True)
+#     like_sum = models.IntegerField(default=0)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def save(self, *args, **kwargs):
+#         img = Image.open(self.art)
+#         img = img.convert('RGB')
+#         output = BytesIO()
+#         img.save(output, format='JPEG', quality=20)
+#         output.seek(0)
+#         self.art = File(output, self.art.name.split('.')[0] + '.jpg')
+#         super(Art, self).save(*args, **kwargs)
+
+#     def __str__(self):
+#         return self.title
     
 
 class Comment(models.Model):
