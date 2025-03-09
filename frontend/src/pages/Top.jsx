84135c934840fb6e0838b67510dcff3238f5dc34
kanji <kanji8147@gmail.com>
2025-03-09 13:26:10 +0900

    Containerにflexを適用＆アーティストページへのリンクをListコンポーネントで実装

--- frontend/src/pages/Top.jsx
+++ frontend/src/pages/Top.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from 'react'
 import axios from 'axios';
-import { Container, Box, Typography } from '@mui/material';
+import { Container, Box, Typography, List, ListItem, ListItemText, ListItemButton } from '@mui/material';
 import SquareIcon from '@mui/icons-material/Square';
 
 export const Top = () => {
@@ -21,15 +21,31 @@ export const Top = () => {
 
     return (
         <>
-            <Container>
+            <Container maxWidth="lg" sx={{ mt: 3, display: 'flex', flexDirection: "column", alignItems: 'center', justifyContent: 'center'}}>
                 <Box sx={{ mt: 3, display: 'flex', flexDirection: "column", alignItems: 'center', justifyContent: 'center' }}>
-                    <Typography variant="h4">
+                    <Typography variant="h4" sx={{ mb: 2 }}>
                         <SquareIcon sx={{ mr: 0.5  }}></SquareIcon>Welcome to the ArtMuseum
                     </Typography>
                     <Typography variant="body1">下記のリンクからアーティストのページが見れるよ</Typography>
                 </Box>
-                <Box sx={{ mt: 3 }}>
-                    <ul>
+                <Box sx={{ mt: 3, width: '450px', backgroundColor: 'white', p: 2, boxShadow: 3, borderRadius: 2, display: 'flex', flexDirection: "column", alignItems: 'center', justifyContent: 'center' }}>
+                    <Typography variant="h5" sx={{ display: 'flex', alignItems: 'center' }}>
+                        <SquareIcon sx={{ mr: 0.4, fontSize: '1.2rem' }}></SquareIcon>ARTISTS
+                    </Typography>
+                    <List>
+                        {elements.map((item) => (
+                            <ListItem key={item.id} disablePadding>
+                                <ListItemButton component={Link} to={`/home/${item.id}`} sx={{
+                                    '&:hover': {
+                                        color: 'primary.main',
+                                    }
+                                }}>
+                                    <ListItemText primary={item.username} />
+                                </ListItemButton>
+                            </ListItem>
+                        ))}
+                    </List>
+                    {/* <ul>
                         {elements.map((item) => (
                             <li key={item.id} className="list-row">
                                 <Link 
@@ -50,7 +66,7 @@ export const Top = () => {
                                 </Link>
                             </li>
                         ))}
-                    </ul>
+                    </ul> */}
                 </Box>
             </Container>
         </>
